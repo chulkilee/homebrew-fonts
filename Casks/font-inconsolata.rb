af236d48cf6bc4fc8521ff4f8018b65d9623b3fd
Chulki Lee <chulki@asperasoft.com>
2016-08-31 22:15:46 -0700

    font-inconsolata: use github

--- Casks/font-inconsolata.rb
+++ Casks/font-inconsolata.rb
@@ -1,11 +1,15 @@
 cask 'font-inconsolata' do
-  # version '001.010'
   version :latest
   sha256 :no_check
 
-  url 'http://levien.com/type/myfonts/Inconsolata.otf'
+  # github.com/google/fonts was verified as official when first introduced to the cask
+  url 'https://github.com/google/fonts/trunk/ofl/inconsolata',
+      using:      :svn,
+      trust_cert: true
+  name 'Inconsolata'
   homepage 'http://levien.com/type/myfonts/inconsolata.html'
   license :ofl
 
-  font 'Inconsolata.otf'
+  font 'Inconsolata-Bold.ttf'
+  font 'Inconsolata-Regular.ttf'
 end
